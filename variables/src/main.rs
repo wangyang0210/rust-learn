c4403ff6a17747cf8c34f06f0c3280140477a583
wangyang <i@easybe.org>
2024-06-29 07:04:54 +0800

    chore(const): const

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,4 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}")
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
 }
