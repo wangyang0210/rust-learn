771912acdd97456d8596252f2147467cb6fc0585
wangyang <i@easybe.org>
2024-07-07 21:44:54 +0800

    chore(function): function

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -108,4 +108,16 @@
 //     println!("first: {first}, second: {second}")
 // }
 
+use std::io;
 // 无效的数组访问
+fn  main() {
+    let a = [1,2,3,4,5,6];
+    println!("Please enter an array index：");
+    let mut index = String::new();
+    io::stdin().read_line(&mut index)
+        .expect("Failed to read line");
+    let index: usize = index.trim().parse()
+        .expect("Index entered was not a number");
+    let element = a[index];
+    println!("The value of the element at index {index} is: {element}");
+}
