b98cdf18c65c88d8c924d2638bde693376fbf3d1
wangyang <i@easybe.org>
2024-07-26 12:49:25 +0800

    chore(05): borrowing

--- 04_references_borrowing/src/main.rs
+++ 04_references_borrowing/src/main.rs
@@ -1,11 +1,20 @@
+// fn main() {
+//     let s1 = String::from("hello");
+//
+//     let len = calculate_length(&s1);
+//
+//     println!("The length of '{}' is {}.", s1, len);
+// }
+//
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+// }
+
 fn main() {
-    let s1 = String::from("hello");
-
-    let len = calculate_length(&s1);
-
-    println!("The length of '{}' is {}.", s1, len);
+    let s = String::from("hello");
+    change(&s);
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn change(some_string: &String) {
+    some_string.push_str(", world");
 }
