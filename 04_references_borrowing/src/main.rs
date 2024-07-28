ea4fcfad12e4482c66ce9101f36dd05fabb17af5
wangyang <i@easybe.org>
2024-07-28 17:52:14 +0800

    chore(05): references

--- 04_references_borrowing/src/main.rs
+++ 04_references_borrowing/src/main.rs
@@ -10,11 +10,21 @@
 //     s.len()
 // }
 
+// fn main() {
+//     let s = String::from("hello");
+//     change(&s);
+// }
+//
+// fn change(some_string: &String) {
+//     some_string.push_str(", world");
+// }
+
 fn main() {
-    let s = String::from("hello");
-    change(&s);
+    let mut s = String::from("hello");
+
+    change(&mut s);
 }
 
-fn change(some_string: &String) {
+fn change(some_string: &mut String) {
     some_string.push_str(", world");
 }
