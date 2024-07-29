4c130ad364370e07db176aecf3314cf0f4021a5b
wangyang <i@easybe.org>
2024-07-29 20:55:43 +0800

    feat(05): 引用和借用

--- 04_references_borrowing/src/main.rs
+++ 04_references_borrowing/src/main.rs
@@ -19,12 +19,55 @@
 //     some_string.push_str(", world");
 // }
 
+// fn main() {
+//     let mut s = String::from("hello");
+//
+//     change(&mut s);
+// }
+//
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
+
+// fn main() {
+//     let mut s = String::from("hello");
+//
+//     let r1 = &s; // no problem
+//     let r2 = &s; // no problem
+//     let r3 = &mut s; // BIG PROBLEM
+//
+//     println!("{}, {}, and {}", r1, r2, r3);
+// }
+
+// fn main() {
+//     let mut s = String::from("hello");
+//
+//     let r1 = &s; // no problem
+//     let r2 = &s; // no problem
+//     println!("{r1} and {r2}");
+//     // variables r1 and r2 will not be used after this point
+//
+//     let r3 = &mut s; // no problem
+//     println!("{r3}");
+// }
+
+// 悬垂引用
+
+// fn main() {
+//     let reference_to_nothing = dangle();
+// }
+
+// fn dangle() -> &String {
+//     let s = String::from("hello");
+//
+//     &s
+// }
+
 fn main() {
-    let mut s = String::from("hello");
-
-    change(&mut s);
+    let reference_to_nothing = no_dangle();
+    println!("{reference_to_nothing}");
 }
-
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+fn no_dangle() -> String {
+    let s = String::from("hello");
+    s
 }
