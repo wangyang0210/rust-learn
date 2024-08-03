28d85dc107a05a82a0f67aec99b33d055dcf3fa7
wangyang <i@easybe.org>
2024-08-03 21:03:54 +0800

    chore(04): slice

--- 04_slice/src/main.rs
+++ 04_slice/src/main.rs
@@ -1,15 +1,23 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+//
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+//
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
+//
+//     s.len()
+// }
+
+
 fn main() {
-    println!("Hello, world!");
-}
-
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return i;
-        }
-    }
-
-    s.len()
+    let s = "hello";
+    let _c = s;
+    let b = s;
+    println!("{}", b)
 }
