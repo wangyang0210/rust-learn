4f6914f3fc9913ee00bd3a02eab441fc56c68ca5
wangyang <i@oyo.cool>
2024-12-12 16:34:06 +0800

    chore(n10): performance

--- n10_01/src/main.rs
+++ n10_01/src/main.rs
@@ -82,13 +82,65 @@
 //     let wont_work = Point { x: 5, y: 4.0 };
 // }
 
-struct Point<T, U> {
-    x: T,
-    y: U,
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
+// 
+// fn main() {
+//     let both_integer = Point { x: 5, y: 10 };
+//     let both_float = Point { x: 1.0, y: 4.0 };
+//     let integer_and_float = Point { x: 5, y: 4.0 };
+// }
+
+
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
+// 
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+// 
+// impl<T> Point<T> {
+//     fn x(&self) -> &T {
+//         &self.x
+//     }
+// }
+// 
+// impl Point<f32> {
+//     fn distance_from_origin(&self) -> f32 {
+//         (self.x.powi(2) + self.y.powi(2)).sqrt()
+//     }
+// }
+// 
+// fn main() {
+//     let p = Point { x: 5, y: 10 };
+// 
+//     println!("p.x = {}", p.x());
+// }
+
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
 }
 
 fn main() {
-    let both_integer = Point { x: 5, y: 10 };
-    let both_float = Point { x: 1.0, y: 4.0 };
-    let integer_and_float = Point { x: 5, y: 4.0 };
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
