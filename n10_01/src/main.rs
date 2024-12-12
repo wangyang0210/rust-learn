5b49f3cdefe20979f9423c8bbad756571dacc289
wangyang <i@oyo.cool>
2024-12-12 16:08:09 +0800

    chore(n10): in enum define

--- n10_01/src/main.rs
+++ n10_01/src/main.rs
@@ -36,28 +36,59 @@
 //     assert_eq!(*result, 'y');
 // }
 
-
+//// error
 // fn largest<T>(list: &[T]) -> &T {
-fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
+//// fix
+// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
+// 
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+// 
+//     largest
+// }
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+// 
+//     let result = largest(&number_list);
+//     println!("The largest number is {result}");
+// 
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+// 
+//     let result = largest(&char_list);
+//     println!("The largest char is {result}");
+// }
 
-    largest
+
+// #[derive(Debug)]
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// fn main() {
+//     let integer = Point { x: 5, y: 10 };
+//     let float = Point { x: 1.0, y: 4.0 };
+//     println!("{:?}", integer);
+//     println!("{:?}", float);
+// }
+
+
+// fn main() {
+//     let wont_work = Point { x: 5, y: 4.0 };
+// }
+
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {result}");
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
 }
